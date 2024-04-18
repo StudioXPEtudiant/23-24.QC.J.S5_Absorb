cf543831cfb8415ad09e78f072c86b30bbe660ab
Kryptidd <166767614+Kryptidd@users.noreply.github.com>
2024-04-18 19:27:14 -0400

    le jeu

---
+++ Absorbe/Assets/Scripts/Player/Combat/MeleeAttack.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MeleeAttack : MonoBehaviour
+{
+    [Header("Keybinds")]
+    public KeyCode MeleeKey = KeyCode.Mouse0;
+    
+    public bool onCooldown = false;
+    public float cooldownDuration = 0.5f;
+    public float attackRange = 2.5f;
+    public int activeFrames = 5;
+    public LayerMask enemyLayer;
+
+    private bool connectHit = false;
+
+    public GameObject weapon;
+    private Animator anim;
+    public Transform hitboxPoint;
+
+    private Health _health;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(!onCooldown)
+            {
+                Attack();
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        onCooldown = true;
+        Animator anim = weapon.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+        
+        for(int i = 0; i < activeFrames; i++)
+        {    
+            Collider[] hitEnemies = Physics.OverlapSphere(hitboxPoint.position, attackRange, enemyLayer);
+            
+            foreach(Collider enemy in hitEnemies)
+            {
+                Debug.Log("We hit " + enemy.name);
+                connectHit = true;
+            }
+
+            if(connectHit == true);
+                continue;
+        }
+        StartCoroutine(Cooldown());
+    }
+
+    void onDrawGizmosSelected()
+    {
+        if(hitboxPoint == null)
+            return;
+        
+        Gizmos.DrawWireSphere(hitboxPoint.position, attackRange);
+    }
+
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldownDuration);
+        onCooldown = false;
+    }
+}
